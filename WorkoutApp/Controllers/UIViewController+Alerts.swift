911924050c989f1b01ee75ca779ac71dbfa3c9f0
yazzy4 <yazzy4@gmail.com>
2019-03-05 12:45:50 -0500

    Added new files, working on UI

---
+++ WorkoutApp/Controllers/UIViewController+Alerts.swift
@@ -0,0 +1,9 @@
+//
+//  UIViewController+Alerts.swift
+//  WorkoutApp
+//
+//  Created by Yaz Burrell on 3/5/19.
+//  Copyright © 2019 Yaz Burrell. All rights reserved.
+//
+
+import Foundation
---
+++ WorkoutApp/Views/LoginView.swift
@@ -0,0 +1,9 @@
+//
+//  LoginView.swift
+//  WorkoutApp
+//
+//  Created by Yaz Burrell on 3/5/19.
+//  Copyright © 2019 Yaz Burrell. All rights reserved.
+//
+
+import Foundation
